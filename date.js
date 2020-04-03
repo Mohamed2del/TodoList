7651db82abd4ad5b1121d9b4b08fb05ffd3da466
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2020-04-03 22:16:35 +0200

    first commit

---
+++ date.js
@@ -0,0 +1,15 @@
+
+exports.getDate =  function (){
+    const today = new Date();
+        
+    const option = {
+        weekday :"long",
+        day : "numeric",
+        month : "long"
+
+    };
+
+    return today.toLocaleDateString("en-US",option);
+   
+
+}
